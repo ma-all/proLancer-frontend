7027379573b3c3e08dfbc22a917ad3675d1630c9
ma-all <marwaalbaloushi21@gmail.com>
2026-08-26 10:15:02 +0300

    added index function for proposal service

--- src/services/proposal.js
+++ src/services/proposal.js
@@ -15,3 +15,16 @@ const create = async (proposalFormData) => {
         console.log(error)
     }
 }
+
+const index = async () => {
+    try {
+        const res = await fetch(BASE_URL, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`,
+            },
+        })
+        return res.json()
+    } catch (error) {
+        console.log(error)
+    }
+}
