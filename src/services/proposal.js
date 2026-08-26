0856751b1c65932644d75c6d8e7f22c52fc3450d
ma-all <marwaalbaloushi21@gmail.com>
2026-08-26 12:17:59 +0300

    i forgot to add the slash in the base url, so i added it now

--- src/services/proposal.js
+++ src/services/proposal.js
@@ -1,4 +1,4 @@
-const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}projectProposal`;
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/projectProposal`;
 
 const create = async (proposalFormData) => {
     try {
