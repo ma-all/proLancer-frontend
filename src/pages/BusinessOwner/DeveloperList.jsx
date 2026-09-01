47de8a7d18385e079c5fc835d0a170a20d1a9079
Rawan-haji <rawanjan02@gmail.com>
2026-09-01 17:10:19 +0300

    add the search in developer list

--- src/pages/BusinessOwner/DeveloperList.jsx
+++ src/pages/BusinessOwner/DeveloperList.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react"
 import { useNavigate } from "react-router"
-import { User }  from 'lucide-react'
+import { User, Search }  from 'lucide-react'
 
 const DeveloperList = (props) => {
 
-    const developers = props.allDevelopers
+    const developers = props.allDevelopers || []
+    const [searchTerm, setSearchTerm] = useState("")
 
     const navigate = useNavigate()
     
@@ -17,20 +19,38 @@ const DeveloperList = (props) => {
         navigate(`/developer/${developerId}`)
     }
 
+  const filteredDevelopers = developers.filter((dev) => {
+        const usernameMatch = dev.username?.toLowerCase().includes(searchTerm.toLowerCase())
+        const titleMatch = dev.developerTitle?.toLowerCase().includes(searchTerm.toLowerCase())
+        
+        return usernameMatch || titleMatch
+    })
+
     return (
         <div className="developer-page">
             <div className="developer-page-header">
             <h2>Browse Developers</h2>
-            </div>
-            <div className="developers-container">
-            {developers.map((dev) => (
-                <div key={dev._id} className="developer-card">
-                    <div className="icon-wrapper">
-                        <div className="dev-icon">
-                            <User size={25} />
-                            </div>
 
-                    </div>
+           <div className="search-wrapper">
+                    <Search size={18} className="search-icon"/>
+                    <input 
+                        type="text" 
+                        placeholder="Search by name or title.." 
+                        value={searchTerm} 
+                        onChange={(e) => setSearchTerm(e.target.value)} 
+                        className="search-input"
+                    />
+                </div>
+            </div>
+            
+            <div className="developers-container">
+                {filteredDevelopers.map((dev) => (
+                    <div key={dev._id} className="developer-card">
+                        <div className="icon-wrapper">
+                            <div className="dev-icon">
+                                <User size={25} />
+                            </div>
+                        </div>
 
 
                     <h3>{dev.username}</h3>
